ff4890c82cacf4fb7b36023aef03b267c194cc1e
Ameur Mohammed <ameur.medy@gmail.com>
2023-10-23 14:26:51 +0100

    add all new updates

--- six_part.js
+++ six_part.js
@@ -1,42 +1,48 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const dots = document.querySelectorAll(".dot");
-    const cards = document.querySelectorAll(".prt6_card");
-    let currentIndex = 0;
+$(document).ready(function() {
+    let currentCard = 1; // Track the currently displayed card
 
-    // Initialize the first card and dot as active
-    cards[currentIndex].classList.add("active");
-    dots[currentIndex].classList.add("active");
-
-    // Swipe left or right to navigate cards
-    $(".prt6_cards_container").on("swiperight", function() {
-        showCard(currentIndex - 1);
-    });
-
-    $(".prt6_cards_container").on("swipeleft", function() {
-        showCard(currentIndex + 1);
-    });
-
-    // Function to show a specific card
-    function showCard(index) {
-        if (index < 0) {
-            index = cards.length - 1;
-        } else if (index >= cards.length) {
-            index = 0;
-        }
-
-        cards[currentIndex].classList.remove("active");
-        dots[currentIndex].classList.remove("active");
-
-        cards[index].classList.add("active");
-        dots[index].classList.add("active");
-
-        currentIndex = index;
+    // Function to show the specified card and hide others
+    function showCard(cardNumber) {
+      $('.prt6_card').hide();
+      $(`#card${cardNumber}`).show();
     }
 
-    // Click event for navigation dots
-    dots.forEach((dot, index) => {
-        dot.addEventListener("click", () => {
-            showCard(index);
-        });
+    // Function to show the next card
+    function showNextCard() {
+      currentCard = currentCard % 3 + 1; // Loop back to 1 when on the last card
+      showCard(currentCard);
+    }
+
+    // Function to show the previous card
+    function showPrevCard() {
+      currentCard = currentCard - 1 > 0 ? currentCard - 1 : 3; // Loop back to 3 when on the first card
+      showCard(currentCard);
+    }
+
+    // Attach click handlers to the navigation buttons
+    $('.nav_left_btn').click(function() {
+      showPrevCard();
     });
-});
+
+    $('.nav_right_btn').click(function() {
+      showNextCard();
+    });
+
+    // Function to handle responsive layout
+    function handleResponsiveLayout() {
+      if ($(window).width() <= 768) {
+        // Screen width is 768px or less
+        $('.navigation-btns').show(); // Show the navigation buttons
+        $('.prt6_card').hide(); // Hide all cards
+        showCard(currentCard); // Show the currently selected card
+      } else {
+        // Screen width is greater than 768px
+        $('.navigation-btns').hide(); // Hide the navigation buttons
+        $('.prt6_card').show(); // Show all cards
+      }
+    }
+
+    // Handle initial layout and window resize
+    handleResponsiveLayout();
+    $(window).resize(handleResponsiveLayout);
+  });
